2ca58d1688518bbc4eb6c22e735bd106833cbb7f
Jacob Fought <jhfought@asu.edu>
2020-10-07 21:51:30 -0700

    Initial version.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,31 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//Returns total.
+	public int getTotal () {
+		return 0;
+	}
+	
+	//Adds to value to total.
+	public void add (int value) {
+	}
+	
+	//Subtracts value from total.
+	public void subtract (int value) {
+	}
+	
+	//Outputs total as a String.
+	public String toString () {
+		return "";
+	}
+	
+	//Clears total.
+	public void clear() {
+	}
+}
