b913a7998ec71b5b425ea728bd2b0c234197e8ad
Jacob Fought <jhfought@asu.edu>
2020-10-07 22:18:03 -0700

    Functionality added.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,30 +2,38 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	//Returns total.
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	//Adds to value to total.
 	public void add (int value) {
+		this.total += value;
+		this.history += " + " + value;
 	}
 	
 	//Subtracts value from total.
 	public void subtract (int value) {
+		this.total -= value;
+		this.history += " - " + value;
 	}
 	
-	//Outputs total as a String.
+	//Returns history of total.
 	public String toString () {
-		return "";
+		return this.history;
 	}
 	
-	//Clears total.
+	//Clears AddingMachine.
 	public void clear() {
+		this.total = 0;
+		this.history = "0";
 	}
 }
